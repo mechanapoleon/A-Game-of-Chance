7caae13f78a8cdfe67082a8fae158f7e8cd0cd2f
mechanapoleon <michelsanchez.ms1990@gmail.com>
2024-04-04 09:49:42 +0200

    Revert "Revert "Add location cards""
    This reverts commit f52783cf660cece48cb14293ca8b4f4fd795f704.

--- LocationCard.gd
+++ LocationCard.gd
@@ -2,20 +2,31 @@ extends Button
 
 var isFaceUp = false
 var cardContent := "Contenuto della carta" # Sostituisci con il contenuto effettivo della carta
-var cardNumber: int = -1  # Numero unico associato a ciascuna carta, -1 di default
-var card_texture_back = preload("res://Images/LocationCards/loc1back.png")
-var card_texture_front = preload("res://Images/LocationCards/loc1front.png")
 
-
-
+@export var cardNumber: int = -1  # Numero unico associato a ciascuna carta, -1 di default
+@export var card_texture_back: Texture = null
+@export var card_texture_front: Texture = null
 
 func _ready():
-	var upperLeftTexture = $UpperLeftTexture
-	if upperLeftTexture:
+	var cardTexture = $CardTexture
+	if cardTexture:
 		print("Setting initial texture")
-		upperLeftTexture.texture = card_texture_back
-	flipCard()
+		cardTexture.texture = card_texture_back
+		cardTexture.size = Vector2(150, 150)  # Dimensioni visibili
+#	flipCard()
 
+func _on_Button_mouse_entered():
+	if isFaceUp:
+		var cardTexture = $CardTexture
+		if cardTexture:
+			cardTexture.modulate = Color(0.8, 0.8, 0.8)  # Change the texture color when mouse enters
+
+func _on_Button_mouse_exited():
+	if isFaceUp:
+		var cardTexture = $CardTexture
+		if cardTexture:
+			cardTexture.modulate = Color(1, 1, 1)  # Reset the texture color when mouse exits
+			
 func _on_Button_pressed():
 	flipCard()
 
@@ -26,31 +37,19 @@ func flipCard():
 	update_card_visual()
 
 func update_card_visual():
-	# Modifica l'aspetto visivo della carta in base a is_face_up
 	print("Updating card visual")
 	print("isFaceUp: " + str(isFaceUp))
 	if isFaceUp:
-		# Mostriamo il contenuto della carta
-		self.custom_minimum_size = Vector2(75, 75)  # Dimensioni visibili
-		self.modulate = Color(1, 1, 1)  # Colore normale
-		var upperLeftTexture = $UpperLeftTexture
-		if upperLeftTexture:
+		var cardTexture = $CardTexture
+		if cardTexture:
 			print("Setting front texture")
-			upperLeftTexture.texture = card_texture_front
-#			upperLeftTexture.custom_min_size = Vector2(75, 75) #fissa le dimensioni dell'immagine
+			cardTexture.texture = card_texture_front
+			cardTexture.size = Vector2(150, 150)  # Dimensioni visibili
 	else:
-		# Copriamo la carta
-		self.custom_minimum_size = Vector2(75, 75)  # Dimensioni ridotte
-		self.modulate = Color(0.5, 0.5, 0.5)  # Colore grigio scuro o altro colore per indicare copertura
-		$UpperLocationLeftCard.bbcode_text = "Carta " 
-		var upperLeftTexture = $UpperLeftTexture
-		if upperLeftTexture:
+		var cardTexture = $CardTexture
+		if cardTexture:
 			print("Setting back texture")
-			upperLeftTexture.texture = card_texture_back
-#			upperLeftTexture.custom_min_size = Vector2(75, 75) #fissa le dimensioni dell'immagine
+			cardTexture.texture = card_texture_back
+			cardTexture.size = Vector2(150, 150)  # Dimensioni visibili	
 
 		# Puoi anche aggiungere un'animazione qui
-
-# Nuovo metodo per impostare il numero della carta
-func set_card_number(number: int):
-	cardNumber = number
